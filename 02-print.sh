2f48873ff02362381006df7b6f24715f666917aa
nayin <santoshnainwar9505@gmail.com>
2023-06-28 17:15:59 +0530

    first shell script

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,14 @@
 #to print here are couple of command,but echo is widely used one
 
 echo Hello World
+
+#syntax: echo -e "\e[COLmMESSAGE\e[0m"
+#red(31),green(32),yellow(33),blue(34),magenta(35),cyan(36)
+
+echo -e "\e[31mHello in RED color\e[0m"
+echo -e "\e[32mHello in GREEN color\e[0m"
+echo -e "\e[33mHello in YELLOW color\e[0m"
+echo -e "\e[34mHello in BLUE color\e[0m"
+echo -e "\e[35mHello in MAGENTA color\e[0m"
+echo -e "\e[36mHello in CYAN color\e[0m"
+
